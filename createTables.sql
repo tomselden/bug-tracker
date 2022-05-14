eb09924219eb97693aa940244bae7e9a13300144
Thomas Selden <thomasselden42@gmail.com>
2022-05-14 17:10:02 -0400

    added part of sql database structure and added config.js file

---
+++ createTables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    email VARCHAR(230) PRIMARY KEY,
+    userName VARCHAR(230),
+    avatar bytea
+);
+
+CREATE TABLE IF NOT EXISTS issues (
+    email VARCHAR(230) PRIMARY KEY,
+    userName VARCHAR(230),
+    
+
+)
