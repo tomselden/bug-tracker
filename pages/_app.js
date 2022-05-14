cdb958f08da46e9514e3cecfb7283135015d2664
Thomas Selden <thomasselden42@gmail.com>
2022-05-14 16:06:06 -0400

    fixed some bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { auth } from "../firebase.js";
-import { useAuthState } from "react-firebase-hooks/auth"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useState } from "react";
+import ls from "local-storage";
 
 function MyApp({ Component, pageProps }) {
   // useAuthState returns array of 3, first is user, second is loading, third is error
   const [user, loading, error] = useAuthState(auth);
-  const [loggedInUser, setLoggedInUser] = useState(null)
-  
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
   useState(() => {
-    setLoggedInUser(ls('self'))
-  }, [])
+    setLoggedInUser(ls("self"));
+  }, []);
 
-  if (!loggedInUser) return <SignIn />;
+  if (!loggedInUser) return (window.location.href = "/login");
 
-  return <Component {...pageProps} />;
+  return (window.location.href = "/profile");
 }
 
 export default MyApp;
